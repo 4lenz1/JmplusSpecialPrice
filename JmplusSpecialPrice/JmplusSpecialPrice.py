517f508d15d92ee1a2f6ff9199ec52c6cbe8e2c7
4lenz1 <pingfallenzone@gmail.com>
2017-01-27 17:33:40 +0900

    fix 亂碼

--- JmplusSpecialPrice/JmplusSpecialPrice.py
+++ JmplusSpecialPrice/JmplusSpecialPrice.py
@@ -1,9 +1,23 @@
-import requests
+#-*- coding: utf-8 -*-　
 import sys
+import requests
+import re
+from  bs4 import BeautifulSoup
 
 
+msg= ''
 res  = requests.get('http://class.ruten.com.tw/user/index00.php?s=jmplus01&d=4797150&o=0&m=1')
+res.encoding ='utf-8' 
+
+soup =BeautifulSoup(res.text , "html.parser") 
+pattern = re.compile('【(.*)】')
+
+for entry in soup.select('.rt-store-goods-disp-mix'):
+    msg+= str(( pattern.findall(entry.select('.item-name')[0].text), 
+              entry.select('.item-direct-price')[0].text[7:].replace(' ', '')))
 
 
 
-print(res.text.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding), file = open('data.txt', 'w'))
+print(msg)
+
+
